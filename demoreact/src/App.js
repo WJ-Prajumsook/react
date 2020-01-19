ed9cea8411da8a7df86ec168b92d5744352640ee
Wijit Prajumsook <wijitp@gmail.com>
2020-01-19 12:36:57 +0100

    add

---
+++ demoreact/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h4 className="bg-primary text-white text-center p-2">
+        Hello world, React.
+      </h4>
+    </div>
+  );
+}
+
+export default App;
